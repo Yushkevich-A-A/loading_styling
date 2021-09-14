00bc97471f84884eebe549974309540823c50066
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-09-14 10:57:41 +0300

    webpack

--- webpack.common.js
+++ webpack.common.js
@@ -55,7 +55,7 @@ module.exports = {
       },
       {
         test: /service.worker\.js$/,
-        loaders: [
+        use: [
           {
             loader: 'file-loader',
             options: {
